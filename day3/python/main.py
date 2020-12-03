80c8087fe5222778972bb615aebf2dcb4bfc9481
Aidan Wolk <aidanwolk@comcast.net>
2020-12-02 21:08:50 -0800

    [day 3] Implement in Python (first pass)

---
+++ day3/python/main.py
@@ -0,0 +1,23 @@
+with open('input.txt') as f:
+    input_file = f.read()
+map = input_file.splitlines()
+
+trees = 0
+
+col = 0
+for row in map:
+    trees += (row[col % len(row)] == '#')
+    col += 3
+
+print(f"Part 1: trees = {trees}")
+
+res = 1
+slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+for dy, dx in slopes:
+    trees = 0
+    col = 0
+    for row in map[::dy]:
+        trees += (row[col % len(row)] == '#')
+        col += dx
+    res *= trees
+print(f"Part 2: product = {res}")
